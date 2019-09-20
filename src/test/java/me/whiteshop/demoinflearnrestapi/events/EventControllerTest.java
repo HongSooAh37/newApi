6f63da80b00ba8033b33c05f21a717858beaaea8
HongSooAh37 <hsa3737@naver.com>
2019-09-20 22:44:30 +0900

    validate wrong date

--- src/test/java/me/whiteshop/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/whiteshop/demoinflearnrestapi/events/EventControllerTest.java
@@ -105,4 +105,27 @@ public class EventControllerTest {
             .andExpect(status().isBadRequest())
         ;
     }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("Spring REST API TEST")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,26,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,25,13,11))
+                .beginEventDateTime(LocalDateTime.of(2018,11,24,01,03))
+                .endEventDateTime(LocalDateTime.of(2018,11,23,15,59))
+                .basePrice(10000)
+                .maxPrice(500)
+                .limitOfEnrollment(100)
+                .location("선릉역 어딘가에 있는 회사에서 고생하는 룸메")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto))
+        )
+                .andExpect(status().isBadRequest())
+        ;
+    }
 }
