b6e507fb20dacb9280415586e2753235a855bb07
HongSooAh37 <hsa3737@naver.com>
2019-10-13 23:13:31 +0900

    Test Exception

--- src/main/java/me/whiteshop/demoinflearnrestapi/accounts/Account.java
+++ src/main/java/me/whiteshop/demoinflearnrestapi/accounts/Account.java
@@ -6,11 +6,11 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Getter @Setter @EqualsAndHashCode(of ="id")
+@Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @NoArgsConstructor @AllArgsConstructor
 public class Account {
 
-    @Id @Generated
+    @Id @GeneratedValue
     private Integer id;
 
     private String email;
---
+++ src/main/java/me/whiteshop/demoinflearnrestapi/accounts/AccountRepository.java
@@ -0,0 +1,17 @@
+package me.whiteshop.demoinflearnrestapi.accounts;
+
+/*
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import java.util.Optional;
+//
+//public interface AccountRepository extends JpaRepository<Account,Integer> {
+//    Optional<Account> findByEmail(String username);
+//}
+*/
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByEmail(String username);
+}
